09fca2e1267aead95e35d02b441e5a8fc42e09f4
Damir Ablikhanov <damir.ablikhanov@mail.ru>
2024-07-26 11:17:50 +0500

    Check

--- src/users/createUser.dto.ts
+++ src/users/createUser.dto.ts
@@ -1,4 +1,4 @@
-import { Transform } from 'class-transformer';
+import { Transform } from 'class-transformer'
 import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
